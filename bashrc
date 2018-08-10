5e299698df877a22b191fb9a8307d239e3be1e29
agaric <agaric@protonmail.com>
2018-08-10 19:48:34 +0200

    revert to guixsd and replace dell xps with asus c302

--- bashrc
+++ bashrc
@@ -8,17 +8,28 @@ HISTFILE="$HOME/.bashlog"
 HISTFILESIZE=8192
 HISTSIZE=8192
 
-export PATH="$HOME/bin${PATH:+:}$PATH"
+export PATH="$HOME/bin:$HOME/.guix-profile/sbin${PATH:+:}$PATH"
+export CMAKE_PREFIX_PATH="$HOME/.guix-profile${CMAKE_PREFIX_PATH:+:}$CMAKE_PREFIX_PATH"
+export CPATH="$HOME/.guix-profile/include${CPATH:+:}$CPATH"
+export GHC_PACKAGE_PATH="$HOME/.guix-profile/lib/ghc-8.0.2/package.conf.d${GHC_PACKAGE_PATH:+:}$GHC_PACKAGE_PATH"
+export GIT_EXEC_PATH="$HOME/.guix-profile/libexec/git-core"
+export GIT_SSL_CAINFO="$HOME/.guix-profile/etc/ssl/certs/ca-certificates.crt"
+export LIBRARY_PATH="$HOME/.guix-profile/lib:$HOME/.guix-profile/lib64${LIBRARY_PATH:+:}$LIBRARY_PATH"
+export PKG_CONFIG_PATH="$HOME/.guix-profile/lib/pkgconfig${PKG_CONFIG_PATH:+:}$PKG_CONFIG_PATH"
+export PYTHONPATH="$HOME/.guix-profile/lib/python3.6/site-packages${PYTHONPATH:+:}$PYTHONPATH"
 export LESS=-imRS
 export LESSHISTFILE=-
-export GTK_IM_MODULE=uim
-export QT_IM_MODULE=uim
-export XMODIFIERS='@im=uim'
+export XDG_DATA_DIRS="$HOME/.guix-profile/share/glib-2.0/schemas${XDG_DATA_DIRS:+:}$XDG_DATA_DIRS"
+export XDG_RUNTIME_DIR="/tmp/runtime-$USER"
 
+[[ -d $XDG_RUNTIME_DIR ]] || mkdir -p $XDG_RUNTIME_DIR
 B_TMP="/tmp/_${USER}_tmp"
+! pgrep -u "$USER" ssh-agent >/dev/null \
+  && mkdir -p "$B_TMP" \
+  && ssh-agent | grep -v echo >"$B_TMP/ssh-agent"
+[[ -z "$SSH_AGENT_PID" ]] && eval "$(<$B_TMP/ssh-agent)"
 eval $(dircolors --sh "$HOME/.config/dircolors")
-shopt -s checkwinsize
-#stty -ixon # stop freezing in vim when ctrl-s
+stty -ixon # stop freezing in vim when ctrl-s
 trap 'echo -ne "\e]2;$BASH_COMMAND\a"' DEBUG # dynamic title
 
 alias cp='cp -iv'
@@ -30,18 +41,21 @@ alias ll='ls -lh'
 alias lla='ll -a'
 alias lls='ll -S'
 alias llt='ll -t'
+alias am=alsamixer
 alias bc='rlwrap bc'
 alias diff='diff --color'
 alias dmesg='dmesg --color=always'
 alias e='emacsclient -nc'
 alias g=git
 alias guile='rlwrap guile'
-#alias lein='rlwrap lein'
+alias lein='rlwrap lein'
 alias pstree='pstree -hnp'
-#alias rg='rg --colors match:fg:black --colors match:style:nobold --colors path:style:bold --colors line:fg:yellow'
+alias rg='rg --colors match:fg:black --colors match:style:nobold --colors path:style:bold --colors line:fg:yellow'
+alias sbcl='rlwrap sbcl'
 alias tclsh='rlwrap tclsh'
 alias sudo='sudo '
 alias v=vim
+alias vlc=0vlc
 alias 0clock='echo "$(date +%s)"; echo "  UTC:       $(TZ=UTC date)"; echo "* Prague:    $(date)"; echo "  London:    $(TZ=Europe/London date)"; echo "  Los Angls: $(TZ=America/Los_Angeles date)"; echo "  New York:  $(TZ=America/New_York date)"; echo "  Riyadh:    $(TZ=Asia/Riyadh date)"; echo "  Seoul:     $(TZ=Asia/Seoul date)"'
 alias 0fonts="fc-list | sed 's/^.\+: //;s/:.\+$//;s/,.*$//' | sort -u | pr -2 -T"
 alias 0ip='wget -qO - https://ipinfo.io/ip'
---
+++ guix/c.scm
@@ -0,0 +1,369 @@
+(define-module (my packages)
+  #:use-module (gnu packages pkg-config)
+  ; my-linux
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages linux)
+  ; my-libgestures
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages serialization)
+  ; my-cmt
+  #:use-module (gnu packages xorg)
+
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-system trivial)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix packages))
+
+(define-public my-firmware
+  (package
+   (name "my-firmware")
+   (version "7b5835fd37630d18ac0c755329172f6a17c1af29")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "git://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git")
+           (commit version)))
+     (sha256 (base32 "111pk6867an94gqshbyrbghsn9pbzjhgswdcjwd4z7cl7ikh6q0p"))))
+   (build-system trivial-build-system)
+   (arguments
+    `(#:modules ((guix build utils))
+      #:builder
+      (begin
+        (use-modules (guix build utils))
+        (let* ((source (assoc-ref %build-inputs "source"))
+               (fw-dir (string-append %output "/lib/firmware/"))
+               (i915-dir (string-append fw-dir "i915/"))
+               (intel-dir (string-append fw-dir "intel/")))
+          (mkdir-p fw-dir)
+          (mkdir-p i915-dir)
+          (mkdir-p intel-dir)
+          (for-each (lambda (file)
+                      (copy-file file (string-append fw-dir (basename file))))
+                    (find-files source "iwlwifi-7.*\\.ucode$"))
+          (for-each (lambda (file)
+                      (copy-file file (string-append i915-dir (basename file))))
+                    (find-files source "skl.*\\.bin$"))
+          (for-each (lambda (file)
+                      (copy-file file (string-append intel-dir (basename file))))
+                    (find-files source "dsp_fw_rel.*"))
+          (for-each (lambda (file)
+                      (copy-file file (string-append fw-dir (basename file))))
+                    (find-files source (string-append
+                                        "LICENSE\\.i915$"
+                                        "|LICENCE\\.iwlwifi")))
+          #t))))
+   (home-page "https://wireless.wiki.kernel.org/en/users/drivers/iwlwifi")
+   (synopsis "blobby firmware")
+   (description "blobby firmware")
+   (license (license:non-copyleft "https://git.kernel.org/cgit/linux/kernel/git/firmware/linux-firmware.git/tree/LICENSE.iwlwifi_firmware?id=HEAD"))))
+
+(define-public my-audio-firmware
+  (package
+   (name "my-audio-firmware")
+   (version "278f9da5b888ae32b926c04794731a2ea241d892")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+           (url "https://git.codentium.com/StephanvanSchaik/gentoo-chromebook-skylake.git")
+           (commit version)))
+     (sha256 (base32 "1hi66ilkyy92p64ix3q3wp1ljk6kzq88z4b2f39vf5y1mnxlb7w4"))))
+   (build-system trivial-build-system)
+   (arguments
+    `(#:modules ((guix build utils))
+      #:builder
+      (begin
+        (use-modules (guix build utils))
+        (let* ((source (assoc-ref %build-inputs "source"))
+	       (fw-dir (string-append %output "/lib/firmware/")))
+          (mkdir-p fw-dir)
+          (for-each (lambda (file)
+                      (copy-file file (string-append fw-dir (basename file))))
+                    (find-files source "dfw_sst\\.bin$"))
+          #t))))
+   (home-page "https://www.codentium.com/gentoo-on-skylake-based-chromebooks.html")
+   (synopsis "support for linux on skylake-based chromebooks")
+   (description "support for linux on skylake-based chromebooks")
+   (license (license:non-copyleft ""))))
+
+(define my-linux-config
+  (string-append (dirname (current-filename)) "/c.kconfig"))
+
+(define-public my-linux
+  (package
+   (inherit linux-libre)
+   (name "my-linux")
+   (version "4.18.1")
+   (source
+    (origin
+     (method url-fetch)
+     (uri (list "https://www.kernel.org/pub/linux/kernel/v4.x/linux-4.18.1.tar.xz"))
+     (sha256 (base32 "0hps1h8rs1cc8385m69754vwbjmwasr7bfv9f9nsv8fmx73aspvj"))))
+   (native-inputs
+    `(("kconfig" ,my-linux-config)
+      ,@(alist-delete "kconfig" (package-native-inputs linux-libre))))
+   (inputs
+    `(("lz4" ,lz4)
+      ,@(package-inputs linux-libre)))))
+
+(define-public my-libevdevc
+  (package
+   (name "my-libevdevc")
+   (version "05f67cb94888f3d9f97b5557caf4081543e8ba0e")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+	   (url "https://github.com/hugegreenbug/libevdevc.git")
+	   (commit version)))
+     (sha256 (base32 "0jnjyzh5ncdal6f125q4i5k6s7pd6ca3yha92d5prqfganlr3apd"))))
+   (build-system gnu-build-system)
+   (arguments
+    `(#:tests? #f
+      #:phases (modify-phases %standard-phases
+                 (replace 'configure
+                   (lambda* (#:key outputs #:allow-other-keys)
+                     (substitute* "common.mk"
+                       (("/bin/echo") (which "echo")))
+                     (substitute* "include/module.mk"
+                       (("\\$\\(DESTDIR\\)/usr/")
+			(string-append (assoc-ref outputs "out") "/")))
+                     (substitute* "src/module.mk"
+                       (("\\$\\(DESTDIR\\)")
+			(string-append (assoc-ref outputs "out") "/"))))))))
+   (home-page "https://github.com/hugegreenbug/libevdevc")
+   (synopsis "chromiumos libevdev for linux")
+   (description "chromiumos libevdev for linux")
+   (license license:bsd-3)))
+
+(define-public my-libgestures
+  (package
+   (name "my-libgestures")
+   (version "7a91f7cba9f0c5b6abde2f2b887bb7c6b70a6245")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+	   (url "https://github.com/hugegreenbug/libgestures.git")
+	   (commit version)))
+     (sha256 (base32 "03wg0jqh9ilsr9vvqmakg4dxf3x295ap2sbq7gax128vgylb79i7"))))
+   (build-system gnu-build-system)
+   (native-inputs
+    `(("glib" ,glib)
+      ("pkg-config" ,pkg-config)))
+   (inputs
+    `(("jsoncpp" ,jsoncpp)))
+   (arguments
+    `(#:tests? #f
+      #:phases (modify-phases %standard-phases
+                 (replace 'configure
+                   (lambda* (#:key outputs #:allow-other-keys)
+                            (substitute* "Makefile"
+                                         (("DESTDIR = ")
+                                          (string-append "DESTDIR = " (assoc-ref outputs "out"))))
+                            (substitute* "include/gestures/include/finger_metrics.h"
+                                         (("vector.h\"") "vector.h\"\n#include <math.h>")))))))
+   (home-page "https://github.com/hugegreenbug/libgestures")
+   (synopsis "chromiumos libgestures for linux")
+   (description "chromiumos libgestures for linux")
+   (license license:bsd-3)))
+
+(define-public my-cmt
+  (package
+   (name "my-cmt")
+   (version "711bbcd2dfd67ccdf635ff41d177f1ed1f755bd4")
+   (source
+    (origin
+     (method git-fetch)
+     (uri (git-reference
+	   (url "https://github.com/hugegreenbug/xf86-input-cmt.git")
+	   (commit version)))
+     (sha256 (base32 "162y8v87b9xvv1zk6vj0flx8kpg91n0k9rckjf279imvyy7hz6nw"))))
+   (build-system gnu-build-system)
+   (native-inputs
+    `(("pkg-config" ,pkg-config)))
+   (inputs
+    `(("libevdevc" ,my-libevdevc)
+      ("libgestures" ,my-libgestures)
+      ("xorg-server" ,xorg-server)
+      ("xorgproto" ,xorgproto)))
+   (arguments
+    `(#:configure-flags
+      (list (string-append "--with-sdkdir="
+			   %output
+			   "/include/xorg"))
+      #:phases (modify-phases %standard-phases
+      (add-after 'unpack 'fix-deps
+        (lambda* (#:key inputs #:allow-other-keys)
+          (let ((evd (assoc-ref inputs "libevdevc"))
+                (ges (assoc-ref inputs "libgestures")))
+            (setenv "C_INCLUDE_PATH"
+                    (string-append
+                      (getenv "C_INCLUDE_PATH") ":" evd "/include:" ges "/usr/include"))
+            (setenv "LIBRARY_PATH"
+                    (string-append
+                      (getenv "LIBRARY_PATH") ":" evd "/usr/lib:" ges "/usr/lib"))
+            #t))))))
+   (home-page "https://github.com/hugegreenbug/xf86-input-cmt")
+   (synopsis "chromiumos touchpad driver for linux")
+   (description "chromiumos touchpad driver for linux")
+   (license license:bsd-3)))
+
+(use-modules (gnu) (srfi srfi-1) (my packages))
+(use-system-modules nss)
+(use-service-modules admin cups dbus mcron networking xorg)
+(use-package-modules certs connman cups suckless video wm)
+
+(define %%user "b")
+(define %%host "c")
+(define %%home (string-append "/home/" %%user))
+
+(define my-xorg-modules
+  (cons my-cmt
+        (fold delete %default-xorg-modules
+              '("xf86-input-evdev"
+                "xf86-input-keyboard"
+                "xf86-input-mouse"
+                "xf86-input-synaptics"
+                "xf86-video-ati"
+                "xf86-video-cirrus"
+                "xf86-video-fbdev"
+                "xf86-video-mach64"
+                "xf86-video-nouveau"
+                "xf86-video-nv"
+                "xf86-video-sis"
+                "xf86-video-vesa"))))
+
+(define my-xorg-config "
+Section \"Device\"
+  Identifier \"intel\"
+  Driver \"intel\"
+  Option \"DRI\" \"3\"
+EndSection
+Section \"InputClass\"
+  Identifier \"keyboard\"
+  MatchProduct \"AT Translated Set 2 keyboard\"
+  MatchIsKeyboard \"1\"
+  #Option \"XkbModel\" \"chromebook\"
+EndSection
+Section \"InputClass\"
+  Identifier \"touchpad\"
+  MatchProduct \"Elan Touchpad\"
+  MatchIsTouchpad \"1\"
+  Driver \"cmt\"
+  Option \"AccelerationProfile\" \"-1\"
+  Option \"Australian Scrolling\" \"1\"
+  Option \"Fling To Scroll Enabled\" \"0\"
+  Option \"Pointer Sensitivity\" \"3\"
+  Option \"Pressure Calibration Slope\" \"3.1416\"
+  Option \"Scroll Sensitivity\" \"0\"
+  Option \"Scroll Y Out Scale\" \"0.005\"
+  Option \"Tap Drag Enable\" \"1\"
+  Option \"Tap Minimum Pressure\" \"10\"
+  Option \"Two Finger Scroll Distance Thresh\" \"0\"
+  Option \"Zero Finger Click Enable\" \"0\"
+EndSection
+")
+
+(operating-system
+ (host-name %%host)
+ (timezone "Europe/Prague")
+ (locale "en_US.utf8")
+
+ (bootloader (bootloader-configuration
+              (bootloader grub-efi-bootloader)
+              (target "/boot/efi")))
+
+ (kernel my-linux)
+
+ ; see `filefrag -v /swap`
+ (kernel-arguments (list "resume=UUID=f6c1927d-1570-4465-998c-172131891af3" "resume_offset=1454080"))
+
+ (firmware (append (list my-firmware) %base-firmware)) ; my-audio-firmware causes DMAR error
+
+ (initrd-modules (append (list "mmc_block" "sdhci-pci") %base-initrd-modules))
+
+ (file-systems
+  (cons* (file-system
+          (device (file-system-label "root"))
+          (mount-point "/")
+          (type "ext4"))
+         (file-system
+          (device (file-system-label "EFI"))
+          (mount-point "/boot/efi")
+          (type "vfat"))
+         (file-system
+          (device (file-system-label "data"))
+          (mount-point "/data")
+          (type "ext4"))
+         %base-file-systems))
+
+ (swap-devices (list "/swap"))
+
+ (users
+  (cons (user-account
+         (name %%user)
+         (group "users")
+         (supplementary-groups
+           (list "audio" "kvm" "lp" "netdev" "video" "wheel"))
+         (home-directory %%home))
+        %base-user-accounts))
+
+ (issue "")
+
+ (sudoers-file (plain-file "sudoers"
+                           "root ALL=(ALL) ALL
+%wheel ALL=(ALL) ALL
+b ALL=NOPASSWD: /home/b/bin/0lum, /home/b/bin/0pow, /run/current-system/profile/bin/cmst, /run/current-system/profile/bin/connmanctl, /run/current-system/profile/sbin/halt, /run/current-system/profile/sbin/reboot
+")) ; newline!
+
+ (setuid-programs
+  (cons* #~(string-append #$slock "/bin/slock")
+         %setuid-programs))
+
+ (packages
+  (cons* my-cmt
+         cmst
+         cups
+         cwm
+         libvdpau-va-gl
+         nss-certs
+         xf86-video-intel
+         %base-packages))
+
+ (services
+  (cons* ;(bluetooth-service)
+         (console-keymap-service (string-append %%home "/cfg/hsnt.map.gz"))
+         (dbus-service)
+         ;(dbus-service #:services (list connman))
+         (ntp-service
+          #:servers (list "0.europe.pool.ntp.org"
+                          "1.europe.pool.ntp.org"
+                          "2.europe.pool.ntp.org"
+                          "3.europe.pool.ntp.org"))
+         (polkit-service)
+         (slim-service
+          #:default-user %%user
+          #:auto-login? #t
+          #:startx
+          (xorg-start-command
+            #:configuration-file
+            (xorg-configuration-file
+              #:modules my-xorg-modules
+              #:extra-config (list my-xorg-config))))
+         (service connman-service-type)
+         (service cups-service-type
+                  (cups-configuration
+                    (web-interface? #t)
+                    (default-paper-size "A4")))
+         ;(service mcron-service-type)
+         ;(service rottlog-service-type)
+         (service wpa-supplicant-service-type)
+         (remove (lambda (service)
+                   (eq? (service-kind service) console-font-service-type))
+                 %base-services)))
+
+ (name-service-switch %mdns-host-lookup-nss))

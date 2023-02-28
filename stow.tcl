fe5f8fcf7647178eb869b98106314255aef82567
blobject <agaric@pm.me>
2023-02-28 16:12:57 +0100

    switch to tofi

--- stow.tcl
+++ stow.tcl
@@ -1,18 +1,20 @@
 #! /usr/bin/env tclsh
 
+set d {
+  .config/broot
+  .config/mpv
+  .emacs.d
+  .icons
+  .local/share/applications
+  .themes/0theme/openbox-3
+  bin
+}
 set stowd [file dirname $::argv0]
 if {{relative} eq [file pathtype $::argv0]} {
   set stowd [file normalize "[pwd]/$stowd"]
 }
 cd $::env(HOME)
-exec mkdir -p \
-  .config/broot \
-  .config/mpv \
-  .emacs.d \
-  .icons \
-  .local/share/applications \
-  .themes/0theme/openbox-3 \
-  bin
+exec mkdir -p {*}$d
 cd $stowd
 try {
   exec >/dev/tty stow -v {*}[glob _*]
